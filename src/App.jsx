b9af495478a8a0bb7015c14a9fe5af632c360b57
MousGamee <moussa.sagna@hotmail.fr>
2024-01-19 22:53:01 +0100

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Navbar from './components/Nabar'
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Nabar";
+import { Home } from "./pages";
 const App = () => {
   return (
-    <main className='bg-slate-300/20'>
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={'Home'} />
-                <Route path='/' element={'About'} />
-                <Route path='/' element={'Project'} />
-                <Route path='/' element={'Contact'} />
-            </Routes>
-        </Router>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={"About"} />
+          <Route path="/" element={"Project"} />
+          <Route path="/" element={"Home"} />
+        </Routes>
+      </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
